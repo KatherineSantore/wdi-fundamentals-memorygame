99501e3fec171687c3fb1bc171296f80cf8fe8fe
KatherineWard <Katherine.eileen.ward@gmail.com>
2018-05-16 09:12:17 -0400

    "memory game with css"

---
+++ memory_game/js/main.js
@@ -0,0 +1,15 @@
+
+var cards = [ "queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
