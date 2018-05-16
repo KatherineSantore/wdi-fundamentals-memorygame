8db5350eb51c357722e3e19f9f0e321a7d45a135
Katherine Ward <katherine.eileen.ward@gmail.com>
2018-05-16 09:32:07 -0400

    unit 8 changes

---
+++ memory_game/js/main.js
@@ -0,0 +1,20 @@
+
+var cards = [ "queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+		alert ("You found a match!");
+	} else {
+		alert ("Sorry, try again");  
+}
