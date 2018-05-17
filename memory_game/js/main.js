53b4354040f3a949d173ab8bf0750d241642a001
Katherine Ward <katherine.eileen.ward@gmail.com>
2018-05-17 16:12:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,19 @@ var cards = [ "queen", "queen", "king", "king"]
 
 var cardsInPlay = []
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king");
-
-<<<<<<< HEAD
-if (cardsInPlay.length === 2 && cardOne === cardTwo) {
-		alert ("You found a match!");
-	} else {
-		alert ("Sorry, try again");  
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
-=======
->>>>>>> 99501e3fec171687c3fb1bc171296f80cf8fe8fe
+}
+
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
